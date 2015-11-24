7b562a9ea53ef983cdda954ae07fd751cb06e6c6
André Gonçalves <andreslb1@gmail.com>
2015-11-24 12:25:12 +0100

    Add implementation for quantity field

--- plonetheme/tm/browser/js/theme.js
+++ plonetheme/tm/browser/js/theme.js
@@ -3,6 +3,27 @@
     T E Y L E R S
 --------------------------------------------------------------------------------- */
 
+
+function updateQuantity(way){
+    var quantity = parseInt($('div.buyable input.cart_item_count').val());
+
+    if (way == 'up'){
+        if (quantity < 448){
+            quantity++;
+        } else {
+            quantity = 448;
+        }
+    } else {
+        if (quantity > 1){
+            quantity--;
+        } else {
+            quantity = 1;
+        }
+    }
+
+    $('div.buyable input.cart_item_count').val(quantity);
+}
+
 $(document).ready(function() {
       
 });
