1e5a63b056367bf68e3d15bef936e1e9933a3b90
André Gonçalves <andreslb1@gmail.com>
2016-02-22 17:28:28 +0100

    Fix detailed view

--- plonetheme/tm/browser/js/theme.js
+++ plonetheme/tm/browser/js/theme.js
@@ -24,6 +24,34 @@ function updateQuantity(way){
     $('div.buyable input.cart_item_count').val(quantity);
 }
 
+function replaceCollectionTable(overrides) {
+    var defaults = {
+        "sort_on": "sortable_title",
+        "b_start:int": $("input[name=b_start]").first().val(),
+    };
+    var URL = window.location.href.split('?')[0];
+
+    $("#ajax-spinner").attr("style", "display: block !important;")
+    $.get(URL, $.extend(defaults, overrides), function(data) {
+        var table_collection = $(data).find('#table-collection');
+        $("#table-collection").replaceWith(table_collection);
+        $("#ajax-spinner").attr("style", "display: none !important;")
+    });
+}
+
+$.fn.enableTableCollectionReload = function(selector, overrides, prevent) {
+    var $target = this;
+    if(arguments.count < 3)
+        prevent = false;
+
+    $target.delegate(selector, "click", function(event) {
+        if(prevent)
+            event.preventDefault();
+        replaceCollectionTable(overrides);
+    });
+    return $target;
+};
+
 $(document).ready(function() {
     if ($("body").hasClass('template-advancedsearch')) {
         $("#advanced_search_form").submit(function() {
@@ -33,6 +61,13 @@ $(document).ready(function() {
                 }
             });
         });
+    }
+
+    if ($("body").hasClass('template-detailed_tabular_view')) {
+        var ccore = $("#content-core");
+        ccore.enableTableCollectionReload("#stock-column", { "sort_on": "product_stock"});
+        ccore.enableTableCollectionReload("#title-column", { "sort_on": "sortable_title"});
+        ccore.enableTableCollectionReload("#articleNumber-column", { "sort_on": "product_articleNumber"});
     }
 });
 
